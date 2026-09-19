d23e2ccf1507c00bec929729480eb4f41782cf90
Srini1504 <2022wa86713@wilp.bits-pilani.ac.in>
2026-09-19 12:25:23 +0530

    Modified App.java to test continuous integration

--- src/main/java/com/bits/App.java
+++ src/main/java/com/bits/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello from 2022WA86713 - Continuous Integration works!" );
     }
 }
